68bf32ec8403c28bfd73f89c8a128167f5cde203
root <root@vmi3156027.contaboserver.net>
2026-04-10 07:06:40 +0200

    Update legacy gold swap helper to ERC1155

--- bsc-genesis-contract/scripts/swap-legacy-gold.js
+++ bsc-genesis-contract/scripts/swap-legacy-gold.js
@@ -3,13 +3,13 @@ const { ethers } = require('ethers');
 const swapAbi = [
   'function legacyGold() view returns (address)',
   'function newGold() view returns (address)',
-  'function swap(uint256 amount)',
+  'function swap(uint256 tokenId, uint256 amount)',
 ];
 
-const erc20Abi = [
-  'function approve(address spender, uint256 amount) returns (bool)',
-  'function allowance(address owner, address spender) view returns (uint256)',
-  'function balanceOf(address account) view returns (uint256)',
+const erc1155Abi = [
+  'function setApprovalForAll(address operator, bool approved)',
+  'function isApprovedForAll(address account, address operator) view returns (bool)',
+  'function balanceOf(address account, uint256 id) view returns (uint256)',
 ];
 
 function required(name) {
@@ -24,33 +24,35 @@ async function main() {
   const provider = new ethers.JsonRpcProvider(required('RPC_URL'));
   const wallet = new ethers.Wallet(required('PRIVATE_KEY'), provider);
   const swapAddress = required('SWAP_CONTRACT_ADDRESS');
+  const tokenId = BigInt(required('TOKEN_ID'));
   const amountWei = required('AMOUNT_WEI');
 
   const swap = new ethers.Contract(swapAddress, swapAbi, wallet);
   const legacyGoldAddress = await swap.legacyGold();
   const newGoldAddress = await swap.newGold();
 
-  const legacyGold = new ethers.Contract(legacyGoldAddress, erc20Abi, wallet);
-  const newGold = new ethers.Contract(newGoldAddress, erc20Abi, wallet);
+  const legacyGold = new ethers.Contract(legacyGoldAddress, erc1155Abi, wallet);
+  const newGold = new ethers.Contract(newGoldAddress, erc1155Abi, wallet);
 
-  const allowance = await legacyGold.allowance(wallet.address, swapAddress);
-  if (allowance < amountWei) {
-    const approveTx = await legacyGold.approve(swapAddress, amountWei);
+  const approved = await legacyGold.isApprovedForAll(wallet.address, swapAddress);
+  if (!approved) {
+    const approveTx = await legacyGold.setApprovalForAll(swapAddress, true);
     console.log(`approve tx: ${approveTx.hash}`);
     await approveTx.wait();
   }
 
-  const beforeLegacy = await legacyGold.balanceOf(wallet.address);
-  const beforeNew = await newGold.balanceOf(wallet.address);
+  const beforeLegacy = await legacyGold.balanceOf(wallet.address, tokenId);
+  const beforeNew = await newGold.balanceOf(wallet.address, tokenId);
 
-  const swapTx = await swap.swap(amountWei);
+  const swapTx = await swap.swap(tokenId, amountWei);
   console.log(`swap tx: ${swapTx.hash}`);
   await swapTx.wait();
 
-  const afterLegacy = await legacyGold.balanceOf(wallet.address);
-  const afterNew = await newGold.balanceOf(wallet.address);
+  const afterLegacy = await legacyGold.balanceOf(wallet.address, tokenId);
+  const afterNew = await newGold.balanceOf(wallet.address, tokenId);
 
   console.log({
+    tokenId: tokenId.toString(),
     legacyBefore: beforeLegacy.toString(),
     legacyAfter: afterLegacy.toString(),
     newBefore: beforeNew.toString(),
